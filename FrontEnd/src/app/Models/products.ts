d6fc9135f81095848dea196c0008332b76965d73
silindaM <silindanemdu@gmail.com>
2021-09-09 22:13:53 +0200

    product class

---
+++ FrontEnd/src/app/Models/products.ts
@@ -0,0 +1,9 @@
+export class Products {
+  id?:number;
+  productName?:String;
+  ProductPrice?:number;
+  ProductQuantity?:number;
+  ProductDescription?:String;
+  ProductImage?:String;
+  categoryName?:String;  
+}
