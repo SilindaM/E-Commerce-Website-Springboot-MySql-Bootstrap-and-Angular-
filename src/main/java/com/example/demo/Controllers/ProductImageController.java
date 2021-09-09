657499cb6b82cfab63ca7ca356307264f21b1128
silindaM <silindanemdu@gmail.com>
2021-09-09 22:42:12 +0200

    image controller

---
+++ src/main/java/com/example/demo/Controllers/ProductImageController.java
@@ -0,0 +1,10 @@
+package com.example.demo.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductImageController {
+
+	@Autowired
+}
